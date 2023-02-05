6b1dbd44861d07b576cb0b5503b14ce44c467b5c
DARKRAVEN192 <kornienko.nikita@gmail.com>
2023-02-05 21:10:22 +0300

    upd 1

--- AsyncAwait_Lesson/ExampleConsole01/Program.cs
+++ AsyncAwait_Lesson/ExampleConsole01/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace ExampleConsole01
 {
@@ -10,6 +11,58 @@ namespace ExampleConsole01
     {
         static void Main(string[] args)
         {
+            //AsyncMath();
+
+            //AsyncWriteBook(); 
+            Task task = AsyncAdd();
+            Console.ReadLine();
+        }
+
+        static void SyncMath()
+        {
+            Math();
+        }
+        
+        static async Task AsyncMath()
+        {
+            await Task.Run(Math);
+            Console.WriteLine("я все сделал");
+        }
+
+        static void Math()
+        {
+            Console.WriteLine("Start counting");
+            Thread.Sleep(5000);
+            Console.WriteLine("counting finished");
+        }
+        static async Task<int> AsyncAdd()
+        {
+            int a = 12;
+            int b = 13;
+            int res = await Task.Run(() => Add(a, b));
+            return res;
+        }
+        static int Add(int a, int b)
+        {
+            Console.WriteLine("Start Adding");
+            Thread.Sleep(5000);
+            Console.WriteLine("Adding finished");
+            return a + b;
+        }
+        static async Task AsyncWriteBook()
+        {
+            string text = "Мое приключение начинается здесь, в лаборатории приключений. " +
+                "Что меня ждет? Надеюсь не стрела в колене";
+            await Task.Run(() => WriteBook(text));
+
+            Console.WriteLine("Я написал книгу");
+        }
+        static void WriteBook(string text)
+        {
+            Console.WriteLine("Start Writing");
+            Thread.Sleep(5000);
+            Console.WriteLine("writing finished");
+            Console.WriteLine($"Text: {text}");
         }
     }
 }
---
+++ AsyncAwait_Lesson/ExampleWPF01/MainWindow.xaml.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net;
+
+
+using ExampleWPF01.Models;
+using System.Diagnostics;
+
+namespace ExampleWPF01
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSync_Click(object sender, RoutedEventArgs e)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            RunSyncLoad();
+
+            stopwatch.Stop();
+            long time = stopwatch.ElapsedMilliseconds;
+            txtBlock.Text += $"Totat time: {time} ms \n\n";
+        }
+
+        private async void Btn_AsyncClick(object sender, RoutedEventArgs e)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            await RunAsyncLoad();
+
+            stopwatch.Stop();
+            long time = stopwatch.ElapsedMilliseconds;
+            txtBlock.Text += $"Totat time: {time} ms \n\n";
+        }
+
+        private async Task RunAsyncLoad()
+        {
+            List<string> sites = PrepareData();
+
+            foreach (var site in sites)
+            {
+                DataModel model = await Task.Run(() => DownloadSite(site));
+                ReportInfo(model);
+            }
+        }
+        private void RunSyncLoad()
+        {
+            List<string> sites = PrepareData();
+
+            foreach (var site in sites)
+            {
+                DataModel model = DownloadSite(site);
+                ReportInfo(model);
+            }
+        }
+
+        private void ReportInfo(DataModel dataModel)
+        {
+            txtBlock.Text += $"Site: {dataModel.Url}, Length: {dataModel.Data.Length}\n";
+        }
+
+        private List<string> PrepareData()
+        {
+            List<string> list = new List<string>()
+            {
+                "https://www.google.ru/",
+                "https://www.youtube.com/",
+                "https://github.com/DARKRAVEN192"
+            };
+
+            return list;
+        }
+
+        private DataModel DownloadSite(string url)
+        {
+            DataModel dataModel = new DataModel();
+
+            dataModel.Url = url;
+
+            //download from internet
+            WebClient client = new WebClient();
+
+            dataModel.Data = client.DownloadString(url);
+
+            return dataModel;
+        }
+    }
+}
